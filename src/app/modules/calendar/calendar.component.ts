fe5b1127222dd67e239771c1c0f5505f5cb32d53
Schita01 <shakocitashvili6@gmail.com>
2023-09-22 03:52:41 +0400

    shako-game

--- src/app/modules/calendar/calendar.component.ts
+++ src/app/modules/calendar/calendar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent {
+  settings:boolean = false
 
+  showSettings() {
+    this.settings = !this.settings
+  }
 }
